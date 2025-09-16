e854a99eb5737f8d9109ce8bbd8e847eedb2415e
elzharrr <thewelut59@gmail.com>
2025-09-16 11:50:27 +0700

    Add files via upload

---
+++ Novel.java
@@ -0,0 +1,71 @@
+package Bu_Pasha;
+
+public class Novel {
+    //property
+    int ISBN;
+    String title;
+    String author;
+    String genre;
+    Double price;
+
+    public Novel() {
+        ISBN = 4714;
+        title = "Berkah Pagi";
+        author = "eel";
+        genre = "Slice of Life";
+        price = 50000.0;
+    }
+
+    //construcktor parameter
+    public Novel(int I, String a, String g, String t, double p) {
+        ISBN = I;
+        title = t;
+        author = a;
+        genre = g;
+        price = p;
+    }
+
+    //getter
+    public int getISBN(){
+        return ISBN;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    //setter
+    public void setISBN(int I){
+        ISBN = I;
+    }
+    public void setTitle(String t) {
+        title = t;
+    }
+    public void setAuthor(String a){
+        author = a;
+    }
+    public void setGenre(String g){
+        genre = g;
+    }
+    public void setPrice(double p){
+        price = p;
+    }
+    public void display(){
+        System.out.println("=====================");
+        System.out.println("ISBN: "+ ISBN);
+        System.out.println("Judul: "+title);
+        System.out.println("Pengarang "+ author );
+        System.out.println("Genre: "+genre);
+        System.out.println("Harga: "+price);
+        System.out.println("=====Terimakasih=====");
+    }
+}
+
---
+++ Noveltest.java
@@ -0,0 +1,10 @@
+package Bu_Pasha;
+
+public class Noveltest {
+    public static void main(String[] args) {
+        Novel berkahpagi = new Novel(4714, "eel", "Slice of Life", "Berkah Pagi", 50000.0 );
+        berkahpagi.display();
+        Novel pamantua = new Novel(1447, "eel", "Slice of Life", "Paman Tua", 50000);
+        pamantua.display();
+    }
+}
